d01e8f7616d3774aec600246069fe63aa1c9d2f6
Huang Guohua <412662584@qq.com>
2023-02-01 21:48:03 +0800

    游戏主场景

---
+++ Assets/Script/Game.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public static Game Instance { get; private set; }
+
+    public Transform ui;
+
+    public MainCanvas mainCanvas;
+    public MsgCanvas msgCanvas;
+
+
+
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(this);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/UI/MainCanvas.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainCanvas : MonoBehaviour
+{
+    public Button btn_start;
+    public Button btn_setting;
+
+    private void Awake()
+    {
+        btn_start.onClick.AddListener(StartGame);
+        btn_setting.onClick.AddListener(ShowSettingCanvas);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartGame()
+    {
+        Debug.Log("start game");
+        var level = PlayerPrefs.GetInt("Level");
+    }
+
+    void ShowSettingCanvas()
+    {
+        Debug.Log("show setting canvas");
+    }
+}
